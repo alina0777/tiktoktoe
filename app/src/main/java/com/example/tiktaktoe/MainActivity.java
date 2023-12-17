2f7278143b413c4a5bfc905b6a10a4c693b9c859
Pavel Kravchenko <pah595787@gmail.com>
2023-12-17 11:30:46 +0200

    Добавление заднего фона, картинки и кнопки в главном меню

--- app/src/main/java/com/example/tiktaktoe/MainActivity.java
+++ app/src/main/java/com/example/tiktaktoe/MainActivity.java
@@ -6,6 +6,8 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -17,18 +19,22 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-        Button startButton = findViewById(R.id.buttonStart);
-        startButton.setOnClickListener(new View.OnClickListener() {
+        Window w = getWindow();
+        w.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        Button buttonStart = findViewById(R.id.buttonStart);
+        buttonStart.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this, GameActivity.class);
                 intent.putExtra("property", "");
                 setResult(Activity.RESULT_OK, intent);
                 startActivityForResult(intent,101);
                 onNewIntent (intent);
-
             }
         });
+
+
 
     }
 }
